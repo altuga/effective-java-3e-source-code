746322cfb062df3071f0a4e499393f1ca646d956
altuga@gmail.com <upux1234>
2018-12-19 17:23:11 +0300

    Work on Item 6

--- src/main/java/effectivejava/chapter2/item6/RomanNumerals.java
+++ src/main/java/effectivejava/chapter2/item6/RomanNumerals.java
@@ -26,20 +26,18 @@ public class RomanNumerals {
 
         int numSets = Integer.parseInt(args[0]);
         int numReps = Integer.parseInt(args[1]);
-        boolean b = false;
+
 
         for (int i = 0; i < numSets; i++) {
             long start = System.nanoTime();
             for (int j = 0; j < numReps; j++) {
-                b ^= isRomanNumeralSlow("MCMLXXVI");  // Change Slow to Fast to see performance difference
+                isRomanNumeralSlow("MCMLXXVI");  // Change Slow to Fast to see performance difference
             }
             long end = System.nanoTime();
             System.out.println(((end - start) / (1_000. * numReps)) + " μs.");
         }
 
-        // Prevents VM from optimizing away everything.
-        if (!b)
-            System.out.println();
+
     }
 }
 
