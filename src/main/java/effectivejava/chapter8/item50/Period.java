ed3cfcc94c83bf3ef8ee4ef7ca8cd7a0a0a0f368
altuga@gmail.com <upux1234>
2018-12-24 22:38:22 +0300

    Item 50 solution is removed

--- src/main/java/effectivejava/chapter8/item50/Period.java
+++ src/main/java/effectivejava/chapter8/item50/Period.java
@@ -31,24 +31,5 @@ public final class Period {
         return start + " - " + end;
     }
 
-//    // Repaired constructor - makes defensive copies of parameters (Page 232)
-//    public Period(Date start, Date end) {
-//        this.start = new Date(start.getTime());
-//        this.end   = new Date(end.getTime());
-//
-//        if (this.start.compareTo(this.end) > 0)
-//            throw new IllegalArgumentException(
-//                    this.start + " after " + this.end);
-//    }
-//
-//    // Repaired accessors - make defensive copies of internal fields (Page 233)
-//    public Date start() {
-//        return new Date(start.getTime());
-//    }
-//
-//    public Date end() {
-//        return new Date(end.getTime());
-//    }
 
-    // Remainder omitted
 }
